90be9bf4eb686cc902f02a095bf75a815600cc47
anamul01988 <anamulhaque0198877@gmail.com>
2023-11-18 23:51:54 +0600

    ssr tested for dynamic id

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -2,7 +2,7 @@ import ProductDetails from "@/components/UI/ProductDetails";
 
 const ProductDetailPage = async ({ params }) => {
   const res = await fetch(`http://localhost:5000/watches/${params.id}`, {
-    cache: "no-store", //server side data fethch holo
+    cache: "no-store", //server side data fethch holo fole ssr holo not ssg
   });
   const data = await res.json();
 
